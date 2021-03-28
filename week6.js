16811be52366ce8b1a28954de0db40044f9bbd78
gerardine <gerardinekaba@gmail.com>
2021-03-28 17:35:33 +0300

    week6

---
+++ week6.js
@@ -0,0 +1,10 @@
+var kiosk={
+fruit:["mango"],
+  getFruit(){
+  return("a list of fruit")+ this.fruit+"";
+     },
+     setFruit(){
+     setFruits();
+       this.fruit=this.fruit+ " "+fruit;
+     }
+     };     console.log(kiosk.fruit);
